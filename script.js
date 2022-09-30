24529cfa358b6f36b7d1c995cbc9c620e282dba0
ronentgreat <ronenh53@gmail.com>
2022-09-30 11:38:43 +0000

    created function for fetch

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ const text_adivce = document.querySelector(".advice");
 const text_id = document.querySelector(".id_advice");
 const advice_url = "https://api.adviceslip.com/advice";
 
-
+const get_advice = () => {
+   fetch(advice_url)
+     .then(function (response) {
+       return response.json();
+     })
+     .then(function (data) {
+       let advice = data.slip.advice;
+       let id_adivce = data.slip.id;
+       text_id.textContent = `Advice # ${id_adivce}`;
+       text_adivce.textContent = `"${advice}"`;
+     })
+     .catch(function () {
+       console.log("Something went wrong :(");
+     });
+}
 
 button.addEventListener("click", () => {
-  fetch(advice_url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-
-      let advice = data.slip.advice;
-      let id_adivce = data.slip.id;
-      text_id.textContent = `Advice # ${id_adivce}`;
-      text_adivce.textContent = `"${advice}"`;
-    })
-    .catch(function () {
-      console.log("Something went wrong :(");
-    });
-  
+ get_advice();
 })
+
+get_advice();
