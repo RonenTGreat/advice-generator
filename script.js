0edd9273495a28f9f2bfc54a7bdd445aaff6f029
ronentgreat <ronenh53@gmail.com>
2022-09-30 12:12:47 +0000

    Updated

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ const text_id = document.querySelector(".id_advice");
 const advice_url = "https://api.adviceslip.com/advice";
 
 const get_advice = () => {
-   fetch(advice_url)
-     .then(function (response) {
-       return response.json();
-     })
-     .then(function (data) {
-       let advice = data.slip.advice;
-       let id_adivce = data.slip.id;
-       text_id.textContent = `Advice # ${id_adivce}`;
-       text_adivce.textContent = `"${advice}"`;
-     })
-     .catch(function () {
-       console.log("Something went wrong :(");
-     });
-}
+  fetch(advice_url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let advice = data.slip.advice;
+      let id_adivce = data.slip.id;
+      text_id.textContent = `Advice # ${id_adivce}`;
+      text_adivce.textContent = `"${advice}"`;
+    })
+    .catch(function () {
+      console.log("Something went wrong :(");
+    });
+};
 
 button.addEventListener("click", () => {
- get_advice();
-})
+  get_advice();
+});
 
 get_advice();
