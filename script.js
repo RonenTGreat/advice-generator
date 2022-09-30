f7e38daf8b0d5aa5309aaa6b0b2e3693ec280105
ronentgreat <ronenh53@gmail.com>
2022-09-30 11:03:14 +0000

    done with fetching advice

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector(".btn");
+const text_adivce = document.querySelector(".advice");
+const text_id = document.querySelector(".id_advice");
+const advice_url = "https://api.adviceslip.com/advice";
+
+
+
+button.addEventListener("click", () => {
+  fetch(advice_url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+
+      let advice = data.slip.advice;
+      let id_adivce = data.slip.id;
+      text_id.textContent = `Advice # ${id_adivce}`;
+      text_adivce.textContent = `"${advice}"`;
+    })
+    .catch(function () {
+      console.log("Something went wrong :(");
+    });
+  
+})
